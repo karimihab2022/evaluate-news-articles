ef799db2b71a8029b4db6d592c30c6503f4891d3
karimihab2022 <85749000+karimihab2022@users.noreply.github.com>
2021-08-31 04:02:23 +0200

    Update URLtest.js

--- src/testing/URLtest.js
+++ src/testing/URLtest.js
@@ -1,1 +1,12 @@
+import { checkURL } from "../client/js/URLch";
 
+describe("Testing the existing of the url", () => {
+  test("Testing the checkURL() function is defined", () => {
+    expect(checkURL).toBeDefined();
+  });
+});  
+describe("Testing the functionality of the url", () => {
+    test("Testing the checkURL() to be a function", () => {
+      expect(typeof checkURL).toBe('function');
+    });  
+});  
